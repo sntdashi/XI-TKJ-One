b4673dc9d808de85b6c615bff255cc2cca2ba78b
gitaa <gitaa@sann>
2024-07-22 11:09:07 +0700

    main

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
 						43
 					</div>
 				</div>
-
+						Woi
 				<div className="grid grid-cols-2 md:grid-cols-2 gap-7 md:gap-5">
 					<div className="grid gap-2 md:gap-0 md:grid-cols-1">
 						<BoxClassIg />
