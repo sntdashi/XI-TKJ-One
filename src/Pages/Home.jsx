46d1af070841918f879bc1e3f66f9776ed1cb487
sntdashi <sntdashi@gmail.com>
2024-07-22 17:53:34 +0700

    main

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
 						</span>
 					</div>
 					<div id="AngkaGradientPink" className="flex items-center justify-center">
-						XII TKJ One
+						XII TKJ 1
 					</div>
 				</div>
 
