3d5eb4a467ae5b0b30176f19150f9ed5b15a0d03
gitaa <gitaa@sann>
2024-07-22 11:19:25 +0700

    main

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -18,14 +18,17 @@ const Home = () => {
 					</div>
 					<div className="">
 						<span id="AngkaGradientBlue" className="text-4xl">
-						TTL Siswa
+						43
 						</span>
 					</div>
 					<div id="AngkaGradientPink" className="flex items-center justify-center">
-						43
+						Siswa
+					</div>
+					<div id="AngkaGradientPink" className="flex items-center justify-center">
+						XII One
 					</div>
 				</div>
-						Woi
+
 				<div className="grid grid-cols-2 md:grid-cols-2 gap-7 md:gap-5">
 					<div className="grid gap-2 md:gap-0 md:grid-cols-1">
 						<BoxClassIg />
