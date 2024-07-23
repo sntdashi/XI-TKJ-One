a9526cd2e75ad1192f58ea1416aadfffae13460c
sntdashi <sntdashi@gmail.com>
2024-07-23 15:47:36 +0700

    main

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
-// Import the functions you need from the SDKs you need
+// Import fungsi yang diperlukan dari SDK Firebase
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-import {getAuth, GoogleAuthProvider} from 'firebase/auth'
-
+// Konfigurasi Firebase Anda
 const firebaseConfig = {
   apiKey: "AIzaSyCgoPSLkdHYLrn4njbdX1JIoSk1KrxFLEA",
   authDomain: "xii-tkj-one.firebaseapp.com",
@@ -12,14 +12,18 @@ const firebaseConfig = {
   projectId: "xii-tkj-one",
   storageBucket: "xii-tkj-one.appspot.com",
   messagingSenderId: "301795205174",
-  appId: "1:301795205174:web:e67d56eb0a8434747fb897",
-  
+  appId: "1:301795205174:web:e67d56eb0a8434747fb897"
 };
 
-// Initialize Firebase
+// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
+
+// Ekspor instance storage dari Firebase
 export const storage = getStorage(app);
 
+// Ekspor instance Firestore dari Firebase
 export const db = getFirestore(app);
+
+// Ekspor instance auth dan GoogleAuthProvider dari Firebase Authentication
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
