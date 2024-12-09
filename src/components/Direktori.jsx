3bfc996eed70ba24f31b35fd18ff98f7d6b9c68d
sntdashi <142691671+sntdashi@users.noreply.github.com>
2024-12-09 23:58:00 +0700

    as

--- src/components/Direktori.jsx
+++ src/components/Direktori.jsx
@@ -11,7 +11,7 @@ const Direktori = () => {
       <div
         style={{
           background: 'rgba(255, 255, 255, 0.5)', 
-          backdropFilter: 'blur(10px)', 
+          backdropFilter: 'blur(5px)', 
           borderRadius: '15px', 
           padding: '15px',
           boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
